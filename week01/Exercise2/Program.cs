44918dc4718b128ea377c9f3b8f7e134e245329c
Brandonnavarro2020 <bavarro45@gmail.com>
2025-05-21 14:58:35 -0500

    done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        string letter = "";
+        string sign = "";
+
+        // Determinar letra
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determinar signo
+        int lastDigit = grade % 10;
+
+        if (letter != "A" && letter != "F")
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+        else
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        // Mensaje de aprobado o no
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Don't worry, keep trying and you'll succeed next time!");
+        }
     }
 }
