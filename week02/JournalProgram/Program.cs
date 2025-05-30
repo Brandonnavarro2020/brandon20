d364f195f4d9983ff33a9032a9c2adc6db908a19
Brandonnavarro2020 <bavarro45@gmail.com>
2025-05-30 16:57:53 -0500

    done

--- week02/JournalProgram/Program.cs
+++ week02/JournalProgram/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Journal journal = new Journal();
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("\nJournal Menu:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display journal");
+            Console.WriteLine("3. Load from file");
+            Console.WriteLine("4. Save to file");
+            Console.WriteLine("5. Exit");
+            Console.Write("Choose an option: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    journal.AddEntry();
+                    break;
+                case "2":
+                    journal.DisplayEntries();
+                    break;
+                case "3":
+                    Console.Write("Enter filename to load: ");
+                    journal.LoadFromFile(Console.ReadLine());
+                    break;
+                case "4":
+                    Console.Write("Enter filename to save: ");
+                    journal.SaveToFile(Console.ReadLine());
+                    break;
+                case "5":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option.");
+                    break;
+            }
+        }
+    }
+}
